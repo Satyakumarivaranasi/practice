fef4048fe964f03046b3b8bfc69dec43ff6f27ee
Satyakumarivaranasi <158685085+Satyakumarivaranasi@users.noreply.github.com>
2024-05-27 17:45:49 +0530

    Add files via upload

---
+++ addusername.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class addingnameex{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your name: ");
+        String username=sc.nextLine();
+        System.out.println("WELCOME "+username);
+    }
+}
