b00cd457f695a0bf3af453635b4ce4965b5336da
Satyakumarivaranasi <158685085+Satyakumarivaranasi@users.noreply.github.com>
2024-05-27 09:32:26 +0530

    Add files via upload

---
+++ usingdecrementoperator.java
@@ -0,0 +1,7 @@
+public class usingdecrementoperator{
+    public static void main(String args[]){
+        for(int i=10;i>0;i--){
+            System.out.println("the decrement values are: "+i);//decrement values from 10 to 1
+        }
+    }
+}
